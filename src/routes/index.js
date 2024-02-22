1dc543b4e5a58c81d400df808fba1362f9202dab
ThGox-06 <thgimenez06@gmail.com>
2024-02-22 05:11:10 -0300

    4 new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,19 @@ const { Router } = require('express');
 const cors = require('cors');
 
 const adminRouter = require('./Admin');
+const baseRateRouter = require('./BaseRate');
+const cityRouter = require('./City');
+const stateRouter = require('./State');
+const countryRouter = require('./Country');
 
 const router = Router();
 
 router.use(cors());
 
 router.use('/admin', adminRouter);
+router.use('/baseRates', baseRateRouter);
+router.use('/city', cityRouter);
+router.use('/state', stateRouter);
+router.use('/country', countryRouter);
 
 module.exports = router;
